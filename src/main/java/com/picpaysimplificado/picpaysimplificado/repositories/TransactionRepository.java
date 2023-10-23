771bae1f5b325a83e983c317cdfe962fe27ef27a
Giordan Garcia <giordangarcia12@gmail.com>
2023-10-23 14:09:43 -0300

    feat: fixed UserRepository and TransactionRepository

--- src/main/java/com/picpaysimplificado/picpaysimplificado/repositories/TransactionRepository.java
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/repositories/TransactionRepository.java
@@ -3,9 +3,11 @@ package com.picpaysimplificado.picpaysimplificado.repositories;
 import com.picpaysimplificado.picpaysimplificado.domain.transaction.Transaction;
 import com.picpaysimplificado.picpaysimplificado.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
 }
