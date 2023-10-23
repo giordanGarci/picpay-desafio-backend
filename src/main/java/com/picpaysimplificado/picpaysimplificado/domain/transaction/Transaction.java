8bb41f7ac352f90e3a08c28e8f6bc979166818aa
Giordan Garcia <giordangarcia12@gmail.com>
2023-10-23 10:28:36 -0300

    feat: create Transaction entity

---
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/domain/transaction/Transaction.java
@@ -0,0 +1,35 @@
+package com.picpaysimplificado.picpaysimplificado.domain.transaction;
+
+import com.picpaysimplificado.picpaysimplificado.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "transactions")
+@Table(name = "transactions")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+
+    private BigDecimal amount;
+
+    private LocalDateTime timestamp;
+}
